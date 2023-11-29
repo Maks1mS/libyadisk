a8ba784397398fbb6d6ecee17a382ccc52371e03
Maxim Slipenko <no-reply@maxim.slipenko.com>
2023-11-29 20:39:27 +0300

    add copyright notice

--- src/yadisk_get_disk_resources.c
+++ src/yadisk_get_disk_resources.c
@@ -1,6 +1,22 @@
-//
-// Created by maxim on 27.11.23.
-//
+/*
+ * libyadisk - A C library for interacting with Yandex.Disk API
+ *
+ * Copyright (C) 2023 Maxim Slipenko
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+ */
+
 #include "yadisk_get_disk_resources.h"
 #include "yadisk_api_internal.h"
 
